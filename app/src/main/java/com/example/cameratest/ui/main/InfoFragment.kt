3f4014d03a5a69f3b29fbf581f71400e6d7fc34e
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-11-19 15:40:36 +0700

    added interface to infofragment

--- app/src/main/java/com/example/cameratest/ui/main/InfoFragment.kt
+++ app/src/main/java/com/example/cameratest/ui/main/InfoFragment.kt
@@ -1,11 +1,15 @@
 package com.example.cameratest.ui.main
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.cameratest.R
+import kotlinx.android.synthetic.main.info_fragment.*
+import kotlinx.android.synthetic.main.main_fragment.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,12 +26,37 @@ class InfoFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //imageView1.setImageResource(R.drawable.ortopedicheskie_zabolevaniya)
+        imageView1.setOnClickListener {
+            setAlert(getString(R.string.info_text_1), "Ортопедия")
+
+        }
+        imageView2.setOnClickListener {
+            setAlert(getString(R.string.info_text_2),"Вытяжение")
+        }
+    }
+    fun setAlert(str:String, nameTitle:String){
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(nameTitle)
+        builder.setMessage(str)
+//builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+            Toast.makeText(context,
+                android.R.string.yes, Toast.LENGTH_SHORT).show()
+        }
+
+        builder.show()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
