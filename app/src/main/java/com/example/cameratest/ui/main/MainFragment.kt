bfdd9c7d1cace6753f78ee9739bafa3c114a2c6a
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-11-21 15:59:28 +0700

    refackt main fragment to new
    design mainFragment + Logic for safe two fotos

--- app/src/main/java/com/example/cameratest/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/cameratest/ui/main/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.cameratest.ui.main
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.media.MediaScannerConnection
 import android.net.Uri
@@ -29,8 +30,10 @@ import java.util.*
 
 class MainFragment : Fragment() {
     private val REQUEST_IMAGE_CAPTURE = 1
-    private var currentPhotoPath: String = ""
-    lateinit var PhotoPath: Uri
+    private var currentPhotoPathL: String = ""
+    private var currentPhotoPathR: String = ""
+    lateinit var PhotoPathL: Uri
+    lateinit var PhotoPathR: Uri
     private val KEY_INDEX = "index"
     val REQUEST_CODE = 100
 
@@ -41,28 +44,45 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         //outState.putBoolean(IS_EDITING_KEY, isEditing)
-        outState.putString(KEY_INDEX, textView1.text as String?)
+        //outState.putString(KEY_INDEX, textView1.text as String?)
     }
 
     override fun onDestroy() {
         super.onDestroy()
     }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
 
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         val s = savedInstanceState?.getString(KEY_INDEX, "1") ?: "1"
+        foot_left.setOnClickListener {
+            dispatchTakePictureIntent(1)
+            galleryAddPic()
+            foot_left.setImageURI(this.PhotoPathL)
+        }
+        foot_right.setOnClickListener {
+            dispatchTakePictureIntent(2)
+            galleryAddPic()
+            foot_right.setImageURI(this.PhotoPathR)
+        }
+        button_help.setOnClickListener {
+            setAlert(R.string.help, "Инструкция")
+        }
+        /*
         button_show_photo.setOnClickListener {
-            if (currentPhotoPath !== ""){
-                imageView.setImageURI(this.PhotoPath)
+            if (currentPhotoPathL !== ""){
+                imageView.setImageURI(this.PhotoPathL)
             }
         }
         button_delete.setOnClickListener {
@@ -75,14 +95,14 @@ class MainFragment : Fragment() {
         button_openG.setOnClickListener {
             openGalleryForImage()
         }
+         */
         button_post.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main){
+            GlobalScope.launch(Dispatchers.Main) {
                 val nameStr = withContext(Dispatchers.Default) {
                     post()
                 }
                 val duration = Toast.LENGTH_SHORT
                 Toast.makeText(context, nameStr, duration).show()
-                textView1.text = nameStr
             }
 
         }
@@ -90,80 +110,115 @@ class MainFragment : Fragment() {
         //galleryAddPic()
 
     }
+    fun setAlert(str: Int, nameTitle: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(nameTitle)
+        builder.setMessage(str)
+//builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+            Toast.makeText(
+                context,
+                android.R.string.yes, Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        builder.show()
+    }
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
-            imageView.setImageURI(data?.data) // handle chosen image
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
+            foot_left.setImageURI(data?.data) // handle chosen image
+        }
+        if (currentPhotoPathL !== "") {
+            foot_left.setImageURI(this.PhotoPathL)
+        }
+        if (currentPhotoPathR !== "") {
+            foot_right.setImageURI(this.PhotoPathR)
         }
     }
-    private fun dispatchTakePictureIntent() {
-        val activity = activity
-        if(activity != null) {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            // Ensure that there's a camera activity to handle the intent
-            takePictureIntent.resolveActivity(activity.packageManager)?.also {
-                // Create the File where the photo should go
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    // Error occurred while creating the File
-                    null
-                }
-                // Continue only if the File was successfully created
 
-                val cont = activity?.applicationContext
-                if (cont != null) {
-                    photoFile?.also {
-                        val photoURI: Uri = FileProvider.getUriForFile(
+    private fun dispatchTakePictureIntent(PhotoPathReal: Int) {
+        val activity = activity
+        if (activity != null) {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                // Ensure that there's a camera activity to handle the intent
+                takePictureIntent.resolveActivity(activity.packageManager)?.also {
+                    // Create the File where the photo should go
+                    val photoFile: File? = try {
+                        createImageFile(PhotoPathReal)
+                    } catch (ex: IOException) {
+                        // Error occurred while creating the File
+                        null
+                    }
+                    // Continue only if the File was successfully created
+
+                    val cont = activity?.applicationContext
+                    if (cont != null) {
+                        photoFile?.also {
+                            val photoURI: Uri = FileProvider.getUriForFile(
                                 cont,
                                 "com.example.android.fileprovider",
                                 it
-                        )
-                        PhotoPath = photoURI
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                        print("Это путь $photoURI")
-                    }
-                }
+                            )
 
-            }
+                            if (PhotoPathReal == 1) {
+                                this.PhotoPathL = photoURI
+                            } else {
+                                this.PhotoPathR = photoURI
+                            }
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                            print("Это путь $photoURI")
+                        }
+                    }
+
+                }
             }
         }
 
     }
 
     @Throws(IOException::class)
-    private fun createImageFile(): File {
+    private fun createImageFile(PhotoPathReal: Int): File {
         // Create an image file name
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val storageDir: File? =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
         //val storageDir1: File? = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
-                "JPEG_${timeStamp}_", /* prefix */
+            "JPEG_${timeStamp}_", /* prefix */
             //"JPEG_my",
-                ".jpg", /* suffix */
-                storageDir /* directory */
+            ".jpg", /* suffix */
+            storageDir /* directory */
         ).apply {
             // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+            if (PhotoPathReal == 1) {
+                currentPhotoPathL = absolutePath
+            } else {
+                currentPhotoPathR = absolutePath
+            }
         }
     }
 
     private fun deletePicture() {
-        if (currentPhotoPath == ""){
+        if (currentPhotoPathL == "") {
             return
         }
-        val f = File(currentPhotoPath)
+        val f = File(currentPhotoPathL)
         f.delete()
-        currentPhotoPath = ""
+        currentPhotoPathL = ""
         val text = R.string.delete_photo
         val duration = Toast.LENGTH_SHORT
         Toast.makeText(context, text, duration).show()
     }
 
-    private fun post():String {
+    private fun post(): String {
         val uploader = activity?.let { UploadUtility(it) }
-        uploader?.uploadFile(currentPhotoPath)
+        uploader?.uploadFile(currentPhotoPathL)
         return ("Все ок")
     }
 
@@ -173,21 +228,23 @@ class MainFragment : Fragment() {
 
         if (cont != null) {
             cont.fileList()
-            val f = File(currentPhotoPath)
+            val f = File(currentPhotoPathL)
             try {
                 activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-                MediaScannerConnection.scanFile(cont, arrayOf(f.toString()),
-                        null, null)
+                MediaScannerConnection.scanFile(
+                    cont, arrayOf(f.toString()),
+                    null, null
+                )
                 //MediaScannerConnection.scanFile(Cont, arrayOf(f.toString()), null, null)
 
-            }
-            catch (e: IOException) {
+            } catch (e: IOException) {
                 // Unable to create file, likely because external storage is
                 // not currently mounted.
                 Log.w("ExternalStorage", "Error writing $f", e)
             }
         }
     }
+
     private fun openGalleryForImage() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
